427639f37b1cae7a4af6891121eeae0962e41bdd
Diego Urrutia-Astorga <mika.cl@gmail.com>
2024-11-28 11:39:46 -0300

    feature: add the Repository and his test

---
+++ app/app/src/test/java/cl/ucn/disc/dsm/pictwin/data/repository/RepositoryTest.kt
@@ -0,0 +1,54 @@
+package cl.ucn.disc.dsm.pictwin.data.repository
+
+import cl.ucn.disc.dsm.pictwin.data.network.NetworkModule
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.slf4j.LoggerFactory
+
+/**
+ * Test: Repository.
+ */
+class RepositoryTest {
+
+    // the logger
+    private var log = LoggerFactory.getLogger(Repository::class.java)
+
+    private lateinit var repository: Repository
+
+    /**
+     * Before each test.
+     */
+    @Before
+    fun setup() {
+        val gson = NetworkModule.provideGson()
+        val api = NetworkModule.provideApiService(gson)
+
+        repository = Repository(api)
+    }
+
+    /**
+     * Testing Authenticate.
+     */
+    @Test
+    fun testAuthenticate() {
+        runBlocking {
+            log.debug("Testing authenticate() ..")
+
+            val result = repository.authenticate("durrutia@ucn.cl", "durrutia123")
+
+            // Optional: Add assertions to verify the result
+            result.onSuccess {
+                log.debug("Authentication successful: {}", it)
+            }.onFailure {
+                log.error("Authentication failed", it)
+                Assert.fail("Authentication failed !!")
+            }
+        }
+    }
+}
+
+
+
+
